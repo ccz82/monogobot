7d5ad33cf94013a4edc367d3b0d91d76f2d01e33
ccz82 <chunzhen82@gmail.com>
2023-07-20 16:32:06 +0800

    feat: improve upgrading
    - tweak threshold value for upgrade indicator matching
    - add templates
    - add print statements

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ STOP_SCRIPT_KEY = keyboard.Key.f1
 PAUSE_SCRIPT_KEY = keyboard.Key.f2
 PATH_TO_TEMPLATES = "templates/"
 FIND_MATCH_THRESHOLD = 0.85
-FIND_ALL_MATCH_THRESHOLD = 0.97
+FIND_ALL_MATCH_THRESHOLD = 0.85
+NEARBY_THRESHOLD_DISTANCE = 50
+
+def is_nearby_coords(coord1, coord2, threshold_distance=NEARBY_THRESHOLD_DISTANCE):
+    x1, y1 = coord1
+    x2, y2 = coord2
+    distance = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance <= threshold_distance
 
 def find_match(template, threshold=FIND_MATCH_THRESHOLD):
     image = cv2.cvtColor(np.array(dialog.capture_as_image()), cv2.COLOR_RGB2GRAY)
@@ -32,7 +39,20 @@ def find_all_matches(template, threshold=FIND_ALL_MATCH_THRESHOLD):
 
     matches = []
     for pt in zip(*loc[::-1]):
-         matches.append((pt[0] + template_width // 2, pt[1] + template_height // 2)) # type: ignore
+        match_coords = (pt[0] + template_width // 2, pt[1] + template_height // 2) # type: ignore
+
+        is_nearby = False
+
+        if not matches:
+            matches.append(match_coords)
+        else:
+            for match in matches:
+                if is_nearby_coords(match, match_coords) or match == match_coords:
+                    is_nearby = True
+                    break
+
+        if not is_nearby:
+            matches.append(match_coords)
 
     return matches
 
@@ -72,39 +92,52 @@ if __name__ == "__main__":
             raise Exception("No templates found!")
 
         upgrade_available = False
+        upgrade_indicators = []
+
         while not end:
             if not paused:
                 if upgrade_available:
+                    print("Entering upgrade menu...")
+
                     start_time = time.time() # Start counting time in upgrade menu
-                    for filename, template in templates.items():
-                        if filename == "upgrade_indicator.png":
-                            upgrade_indicators = find_all_matches(template)
 
-                            while upgrade_available and not paused:
-                                if (time.time() - start_time) > 30: # Exit upgrade menu if 30 seconds elapsed in upgrade menu
-                                    upgrade_available = False
-                                elif not upgrade_indicators:
-                                    upgrade_available = False
-                                else:
-                                    mouse.click(button="left", coords=upgrade_indicators[0]) # Click upgrade indicator
-                                    time.sleep(1.2) # Allow animations to play
+                    if not upgrade_indicators:
+                        print("No upgrade indicators found, attempting to locate.")
+                        upgrade_indicators = find_all_matches(templates["upgrade_indicator.png"])
 
-                                    # Click close button if it pops up
-                                    close_button = find_match(templates["close.png"])
-                                    if close_button:
-                                        mouse.click(button="left", coords=close_button)
+                    print("Upgrade indicators available. Coordinates are")
+                    print(upgrade_indicators)
 
-                                    # Click collect button if it pops up
-                                    collect_button = find_match(templates["collect.png"])
-                                    if collect_button:
-                                        mouse.click(button="left", coords=collect_button)
+                    count = len(upgrade_indicators) - 1
 
-                                    upgrade_indicators.remove(upgrade_indicators[0])
+                    while upgrade_available and not paused and count >= 0:
+                        if (time.time() - start_time) > 30: # Exit upgrade menu if 30 seconds elapsed in upgrade menu
+                            upgrade_available = False
+                        else:
+                            print(f"Clicking upgrade indicator at {upgrade_indicators[count]}")
+                            mouse.click(button="left", coords=upgrade_indicators[count]) # Click upgrade indicator
+                            time.sleep(0.5) # Allow animations to play
+
+                            # Click close button if it pops up
+                            close_button = find_match(templates["close.png"])
+                            if close_button:
+                                mouse.click(button="left", coords=close_button)
+
+                            # Click collect button if it pops up
+                            collect_button = find_match(templates["collect.png"])
+                            if collect_button:
+                                mouse.click(button="left", coords=collect_button)
+
+                            count -= 1
+
+                    upgrade_available = False
 
                 for filename, template in templates.items():
                     coords = find_match(template)
                     if coords:
                         if filename == "upgrade_available.png":
+                            print("Upgrade available, attempting to enter upgrade menu...")
+
                             # Click build button 10 times to ensure user enters upgrade menu
                             for _ in range(10):
                                 mouse.click(button="left", coords=coords)
@@ -112,6 +145,7 @@ if __name__ == "__main__":
                             upgrade_available = True
                             break
                         else:
+                            print(f"Found {filename}, sending click.")
                             mouse.click(button="left", coords=coords)
 
         listener.stop()
